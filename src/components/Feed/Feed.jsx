935da97ae1825d41d15f4e811c1edd0628c3288c
Tayyaba <tayyabahuma12@gmail.com>
2024-04-11 18:15:17 +0500

    created data page

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,13 +1,13 @@
 import "./feed.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import thumbnail1 from "../../assets/thumbnail1.png";
-import thumbnail2 from "../../assets/thumbnail2.png";
-import thumbnail3 from "../../assets/thumbnail3.png";
-import thumbnail4 from "../../assets/thumbnail4.png";
-import thumbnail5 from "../../assets/thumbnail5.png";
-import thumbnail6 from "../../assets/thumbnail6.png";
-import thumbnail7 from "../../assets/thumbnail7.png";
-import thumbnail8 from "../../assets/thumbnail8.png";
+// import thumbnail2 from "../../assets/thumbnail2.png";
+// import thumbnail3 from "../../assets/thumbnail3.png";
+// import thumbnail4 from "../../assets/thumbnail4.png";
+// import thumbnail5 from "../../assets/thumbnail5.png";
+// import thumbnail6 from "../../assets/thumbnail6.png";
+// import thumbnail7 from "../../assets/thumbnail7.png";
+// import thumbnail8 from "../../assets/thumbnail8.png";
 
 import { API_KEY } from "../../data";
 import { Link } from "react-router-dom";
@@ -21,152 +21,26 @@ const Feed = ({ category }) => {
       .then((response) => response.json())
       .then((data) => setData(data.items));
   };
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [category]);
   return (
     <div className="feed">
-      <Link to={`video/20/4521`} className="card">
-        <img src={thumbnail1} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure
-          intounknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </Link>
-      <div className="card">
-        <img src={thumbnail2} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail3} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail4} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail5} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail6} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail7} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail8} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail1} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure
-          intounknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail2} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail3} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail4} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail5} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail6} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail7} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
-      <div className="card">
-        <img src={thumbnail8} alt="" />
-        <h2>
-          The gladdest moment in human life, me thinks, is a departure into
-          unknown lands.
-        </h2>
-        <h3> Richard Burton</h3>
-        <p>15k views &bull; 2 days ago</p>
-      </div>
+      {data.map((item, index) => {
+        return (
+          <Link to={`video/20/4521`} key={index} className="card">
+            <img src={thumbnail1} alt="" />
+            <h2>
+              The gladdest moment in human life, me thinks, is a departure
+              intounknown lands.
+            </h2>
+            <h3> Richard Burton</h3>
+            <p>15k views &bull; 2 days ago</p>
+          </Link>
+        );
+      })}
     </div>
   );
 };
